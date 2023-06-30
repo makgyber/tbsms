c9f47977c8aaee56d98660df084d857b6d790208
Jon Mereria <makgyber@gmail.com>
2023-06-30 16:42:17 +0800

    remove tests

---
+++ app/Filament/Widgets/FailedMessages.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Failed;
+use App\Models\Sent;
+use Closure;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+
+class FailedMessages extends BaseWidget
+{
+    protected function getTableQuery(): Builder
+    {
+        return Failed::query();
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('file'),
+            TextColumn::make('mobile'),
+            TextColumn::make('message')
+        ];
+    }
+
+    protected function getTablePollingInterval(): ?string
+    {
+        return '5s';
+    }
+
+    protected function getTableBulkActions(): array
+    {
+        return [
+            BulkAction::make('retry')
+                ->action(function ($records) {
+                    $path = config('smsd.failed') . '/';
+                    $target = config('smsd.received') . '/';
+                    foreach ($records as $record) {
+                        rename($path . $record['file'], $target . $record['file']);
+                    }
+                }),
+            BulkAction::make('delete')
+                ->action(function ($records) {
+                    $path = config('smsd.failed') . '/';
+                    foreach ($records as $record) {
+                        unlink($path . $record['file']);
+                    }
+                })
+        ];
+    }
+
+    protected function getTableActions(): array
+    {
+        return [
+            DeleteAction::make('delete')
+                ->action(function ($record) {
+                    $path = config('smsd.failed') . '/';
+                    unlink($path . $record['file']);
+                }),
+            Action::make('retry')
+                ->icon('heroicon-o-arrow-up')
+                ->action(function ($record) {
+                    $path = config('smsd.failed') . '/';
+                    $target = config('smsd.received') . '/';
+                    rename($path . $record['file'], $target . $record['file']);
+                })
+        ];
+    }
+}
--- app/Models/Failed.php
+++ app/Models/Failed.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use App\Traits\ReadsSms;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Sushi\Sushi;
 
 class Failed extends Model
 {
-    use HasFactory;
+    use HasFactory, Sushi, ReadsSms;
+
+    public function getRows(): array
+    {
+        $path = config('smsd.failed');
+        return $this->getMessages($path);
+    }
 }
